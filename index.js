13bb4e8d2ec73ff6eb14d7c86348fc50c89bc50a
vaishak <vaishak.k@zohocorp.com>
2020-05-23 11:54:58 +0530

    Added the index.js file

---
+++ index.js
@@ -0,0 +1,41 @@
+require('dotenv').config();
+const fetch = require("node-fetch");
+
+const {
+  TRAKT_ID: traktId,
+  TRAKT_USERNAME: traktUser,
+} = process.env
+
+const API_URL = 'https://api.trakt.tv';
+
+
+async function main() {
+
+  let endpoint = 'stats',
+      API_END = `${API_URL}/users/${traktUser}/${endpoint}`;
+
+   let mainData, mainJson;
+
+
+  try {
+    mainData = await fetch(API_END, {
+      headers: {
+        'Content-Type': 'application/json',
+        'trakt-api-key': traktId,
+        'trakt-api-version': '2',
+      }
+    });
+    mainJson = await mainData.json();
+    console.log(API_END);
+
+  } catch (error) {
+    console.error(`movie-box ran into an issue getting your Trakt.tv data:\n${error}`);
+  }
+}
+
+
+
+
+(async () => {
+  await main();
+})();
